2e89cfe3edf18aa29b2e69fd0fcff8f2e6ffa240
lafetz <yafetyohaness@gmail.com>
2023-12-25 17:10:06 +0300

    update url

--- src/Pages/Context/socketContext.tsx
+++ src/Pages/Context/socketContext.tsx
@@ -25,7 +25,7 @@ const SocketProvider = ({ children }: any) => {
     return <Navigate to="/sigin" />;
   }
 
-  const socket = io("http://localhost:8080", {
+  const socket = io("https://daclan.onrender.com", {
     auth: { token: token },
   });
 
